9c3245c97a05e0d82bba7ac32b6b7238c727b5c2
*** .xyz Labs *** <info@blackholecapital.xyz>
2026-04-21 18:01:30 -0400

    Delete resolver-boundary directory

--- resolver-boundary/index.ts
+++
@@ -1,3 +0,0 @@
-export type { ProductResolver, CartResolver, PaymentResolver } from './contracts';
-export { productResolver } from './productResolver';
-export { ResolverProvider, useResolvers } from './ResolverProvider';
