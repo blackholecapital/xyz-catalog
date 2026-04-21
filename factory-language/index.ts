8bfd42ae016018631d9c583cbb736cf1ec38310f
*** .xyz Labs *** <info@blackholecapital.xyz>
2026-04-21 18:00:51 -0400

    Delete factory-language directory

--- factory-language/index.ts
+++
@@ -1,7 +0,0 @@
-// factory-language/index.ts
-export { ROUTES } from './routes';
-export type { RouteName } from './routes';
-export { SLOTS } from './slots';
-export type { SlotName } from './slots';
-export { TOUCHPOINTS } from './touchpoints';
-export type { TouchpointId } from './touchpoints';
